3e70c6c4e55bb34c59506aa8ed14461df1004a70
MichasPanda <michal.krzyszczuk97@gmail.com>
2017-08-07 17:59:05 +0200

    pon, 7 sie 2017, 17:59:05 CEST

--- detection.cpp
+++ detection.cpp
@@ -22,48 +22,106 @@ int main(int argc, char *argv[])
         Search_Config(argc,argv,config);
         set_config(config);
         int i=1;
-            while(i < argc)
+        if(config.metod=='h')
+            {
+                while(i < argc)
                 {
                     Picture picture1;
                     OutputData output;
                     if(IsPhoto(argv[i]))
                         {
-                        picture1.image=imread(argv[i],CV_LOAD_IMAGE_COLOR);
-                         if(picture1.image.empty())
-                            {
-                                i += 1;
-                                return -1;
-                            }
-                        if(PhotoEditor(picture1,output)==0)
-                            {
-                                if(config.metod=='h')
-                                    {
-                                        output.result_center=Hough_Center(picture1,output);
+                            picture1.image=imread(argv[i],CV_LOAD_IMAGE_COLOR);
+                            if(picture1.image.empty())
+                                {
+                                    i += 1;
+                                    return -1;
+                                }
+                            if(PhotoEditor(picture1,output)==0)
+                                {
+                                output.result_center=Hough_Center(picture1,output);
 
-                                    }
-                                if(config.metod=='w')
-                                    {
-                                        output.result_center=WeightedCenter(picture1,output);
-
-                                    }
-                                if(config.metod=='r')
-                                    {
-                                        //defaultowo ustawiony srodek to srodek prostokata
-                                    }
-                                if(config.metod=='a')
-                                    {
-                                        Point Hough=Hough_Center(picture1,output);
-                                        Point Weighted=WeightedCenter(picture1,output);
-                                        output.result_center += Weighted + Hough;
-                                        output.result_center /= 3;
-                                    }
-                                    output.vector_error = output.result_center - config.slit_center;
-                                    output.norm_of_vector_error = cv::norm(output.vector_error);
-                                    cout<<output.result_center<<endl;
-                            }
+                                }
                         }
                     i += 1;
-                    //cout<<picture1;
                 }
+            }
+        if(config.metod=='w')
+            {
+                while(i < argc)
+                    {
+                        Picture picture1;
+                        OutputData output;
+                        if(IsPhoto(argv[i]))
+                            {
+                                picture1.image=imread(argv[i],CV_LOAD_IMAGE_COLOR);
+                                if(picture1.image.empty())
+                                    {
+                                        i += 1;
+                                        return -1;
+                                    }
+                                if(PhotoEditor(picture1,output)==0)
+                                    {
+                                    output.result_center=WeightedCenter(picture1,output);
+
+                                    }
+                            }
+                    }
+                i += 1;
+            }
+        if(config.metod=='r')
+            {
+                while(i < argc)
+                    {
+                        Picture picture1;
+                        OutputData output;
+                        if(IsPhoto(argv[i]))
+                            {
+                                picture1.image=imread(argv[i],CV_LOAD_IMAGE_COLOR);
+                                if(picture1.image.empty())
+                                    {
+                                        i += 1;
+                                        return -1;
+                                    }
+                                if(PhotoEditor(picture1,output)==0)
+                                    {
+
+                                    }
+                            }
+                        i +=1;
+                    }
+            }
+        if(config.metod=='a')
+            {
+                {
+                    while(i < argc)
+                        {
+                            Picture picture1;
+                            OutputData output;
+                            if(IsPhoto(argv[i]))
+                                {
+                                    picture1.image=imread(argv[i],CV_LOAD_IMAGE_COLOR);
+                                    if(picture1.image.empty())
+                                        {
+                                            i += 1;
+                                            return -1;
+                                        }
+                                    if(PhotoEditor(picture1,output)==0)
+                                        {
+                                            Point Hough=Hough_Center(picture1,output);
+                                            Point Weighted=WeightedCenter(picture1,output);
+                                            output.result_center += Weighted + Hough;
+                                            output.result_center /= 3;
+                                        }
+                                }
+                            i += 1;
+                        }
+
+                }
+
+            }
+        //output.vector_error = output.result_center - config.slit_center;
+        //output.norm_of_vector_error = cv::norm(output.vector_error);
+        //cout<<output.result_center<<endl;
+        //cout<<picture1;
         return 0;
     }
