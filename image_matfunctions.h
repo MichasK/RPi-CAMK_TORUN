7515e8688f7d75fbc3ff71de33be5e2c2b8a04d8
MichasPanda <krzyszczuk.michal@gmail.com>
2018-05-25 12:51:42 +0200

    Git up to date May 2018. part.1

--- image_matfunctions.h
+++
@@ -1,8 +0,0 @@
-#ifndef image_matfunctions_h
-#define image_matfunctions_h
-double Median(std::vector<short int> &array);
-double Average(std::vector<short int> &array);
-double StandardDeviation(std::vector<short int> &array, double average);
-double TreshHold(double standard_deviation, double median);
-std::string GetActualTime(void);
-#endif
